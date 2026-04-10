dfb0b6f5bbd36bb2204777962e01621709158ed6
Moses Karani <karanim594@gmail.com>
2026-04-10 12:22:13 +0300

    upgraded next

--- app/api/_utils/sendEmail.js
+++ app/api/_utils/sendEmail.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import nodemailer from "nodemailer";
 
 let transporter = nodemailer.createTransport({
@@ -11,14 +8,6 @@ let transporter = nodemailer.createTransport({
     user: "info@hornafricainstitute.co.ke",
     pass: process.env.EMAIL_PASSWORD || "",
   },
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages", success);
-  }
 });
 
 export const sendMail = async (mailOptions) => {
